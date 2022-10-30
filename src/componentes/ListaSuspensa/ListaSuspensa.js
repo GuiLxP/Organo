285b6a9d7472c03e8c648e16cff4b0e15ebcc5ec
Guilherme Pantoja <guilhermematheus166@gmail.com>
2022-10-30 11:04:27 -0300

    Escutando eventos

--- src/componentes/ListaSuspensa/ListaSuspensa.js
+++ src/componentes/ListaSuspensa/ListaSuspensa.js
@@ -4,7 +4,7 @@ export const ListaSuspensa = (props) => {
 	return (
 		<div className="lista-suspensa">
 			<label>{props.label}</label>
-			<select>
+			<select required={props.obrigatorio}>
 				{props.item.map((itens) => {
 					return <option key={itens}> {itens}</option>;
 				})}
