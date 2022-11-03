0c585dd6d24cd861ecead0d11b22b0ce6eb5ecf2
GuiLxP <guilhermematheus166@gmail.com>
2022-11-03 16:09:26 -0300

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,44 @@ import Form from "./componentes/Form";
 import Time from "./componentes/Time";
 
 function App() {
+	const times = [
+		{
+			nome: "Programação",
+			corPrimaria: "#57C278",
+			corSecundaria: "#D9F7E9",
+		},
+		{
+			nome: "Front-End",
+			corPrimaria: "#82CFFA",
+			corSecundaria: "#E8F8FF",
+		},
+		{
+			nome: "Data Science",
+			corPrimaria: "#A6D157",
+			corSecundaria: "#F0F8E2",
+		},
+		{
+			nome: "DevOps",
+			corPrimaria: "#E06B69",
+			corSecundaria: "#FDE7E8",
+		},
+		{
+			nome: "Ux e Design",
+			corPrimaria: "#DB6EBF",
+			corSecundaria: "#FAE9F5",
+		},
+		{
+			nome: "Mobile",
+			corPrimaria: "#FFBA05",
+			corSecundaria: "#FFF5D9",
+		},
+		{
+			nome: "Inovação e Gestão",
+			corPrimaria: "#FF8A29",
+			corSecundaria: "#FFEEDF",
+		},
+	];
+
 	const [colaboradores, setColaboradores] = useState([]);
 	const aoNovoColaboradorAdicionado = (colaborador) => {
 		console.log(colaborador);
@@ -18,7 +56,7 @@ function App() {
 					aoNovoColaboradorAdicionado(colaborador)
 				}
 			/>
-			<Time nome="Programação"/>
+			<Time nome="Programação" />
 			<Time nome="Front-End" />
 			<Time nome="Data Science" />
 			<Time nome="Devops" />
