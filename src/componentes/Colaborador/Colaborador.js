b56f316d5c7a09b5637b204a993ec02e66c58700
GuiLxP <guilhermematheus166@gmail.com>
2022-11-04 13:48:08 -0300

    Melhorando componentes e deixando mais fluido

--- src/componentes/Colaborador/Colaborador.js
+++ src/componentes/Colaborador/Colaborador.js
@@ -1,14 +1,14 @@
 import "./Colaborador.css";
 
-export const Colaborador = () => {
+export const Colaborador = (props) => {
 	return (
 		<div className="colaborador">
 			<div className="cabecalho">
-				<img src="https://github.com/GuiLxP.png" alt="" />
+				<img src={props.imagem} alt={props.nome} />
 			</div>
 			<div className="rodape">
-				<h4>Guilherme Pantoja</h4>
-				<h5>Aluno</h5>
+				<h4>{props.nome}</h4>
+				<h5>{props.cargo}</h5>
 			</div>
 		</div>
 	);
--- src/componentes/Time/Time.js
+++ src/componentes/Time/Time.js
@@ -1,13 +1,22 @@
 import { Colaborador } from "../Colaborador/Colaborador";
 import "./Time.css";
 export const Time = (props) => {
-	return (
+	return props.colaboradores.length > 0 ? (
 		<section
 			className="time"
 			style={{ backgroundColor: props.corSecundaria }}>
 			<h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-			<Colaborador />
-			<Colaborador />
+			<div className="colaboradores">
+				{props.colaboradores.map((colaborador) => (
+					<Colaborador
+						nome={colaborador.nome}
+						cargo={colaborador.cargo}
+						imagem={colaborador.imagem}
+					/>
+				))}
+			</div>
 		</section>
+	) : (
+		""
 	);
 };
